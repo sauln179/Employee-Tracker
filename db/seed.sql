5f19657fb14e022dc1603379abc6c971f69b9ba1
sauln179 <79173328+sauln179@users.noreply.github.com>
2022-04-24 09:32:00 -0700

    Add files via upload

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+USE employee_db;
+
+INSERT INTO department (name) VALUES ('Engineer');
+INSERT INTO department (name) VALUES ('Accounting');
+INSERT INTO role (title, salary, department_id) VALUES ('Payroll Specialist', 100000, 0);
+INSERT INTO role (title, salary, department_id) VALUES ('Software Engineer', 200000, 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Richard', 'Smith', 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Nico', 'Hart', 0);
